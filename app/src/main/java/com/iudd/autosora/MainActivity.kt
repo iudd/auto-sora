304cd746737db6a44bbcfefaff602cbea4a903ca
iudd <86305479+iudd@users.noreply.github.com>
2025-10-30 19:48:52 +0800

    Add bottom navigation variable declaration

--- app/src/main/java/com/iudd/autosora/MainActivity.kt
+++ app/src/main/java/com/iudd/autosora/MainActivity.kt
@@ -1,1 +1,5 @@
-import com.google.android.material.bottomnavigation.BottomNavigationView
+    private lateinit var btnBack: ImageButton
+    private lateinit var btnForward: ImageButton
+    private lateinit var btnRefresh: ImageButton
+    private lateinit var fabExtract: FloatingActionButton
+    private lateinit var bottomNavigation: BottomNavigationView
